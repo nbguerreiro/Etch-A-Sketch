b80906aa66908116c22a8b557bb3b9b3f3b40082
nbguerreiro <nbguerreiro@sapo.pt>
2021-03-09 10:42:20 +0000

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+const container = document.querySelector('#container');
+
+for (var i = 1; i <= 256; i++) {
+  let newdiv = document.createElement('div');
+  newdiv.id = "square-div-" + i;
+  container.appendChild(newdiv); 
+}
