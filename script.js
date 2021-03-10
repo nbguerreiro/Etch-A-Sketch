99fba0ebc7625ad5cd9b3ec324205ea58bcc841c
nbguerreiro <nbguerreiro@sapo.pt>
2021-03-10 10:33:57 +0000

    etc

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-
-
-const container = document.querySelector('#container');
+const container = document.querySelector('.container');
 
 for (var i = 1; i <= 256; i++) {
-  let newdiv = document.createElement('div');
-  newdiv.id = "square-div-" + i;
-  container.appendChild(newdiv); 
+    let newdiv = document.createElement('div');
+    newdiv.classList.add('square');
+    container.appendChild(newdiv);
 }
 
+const squares = container.querySelectorAll('.square');
 
+squares.forEach((s) => {
+    s.addEventListener('mouseenter', () => {
+        s.style.backgroundColor = 'black';
+    })
+})
+
+function restart() {
+    console.log("somthing");
+    squares.forEach((s) => {
+        s.style.backgroundColor = null;
+    })
+}
