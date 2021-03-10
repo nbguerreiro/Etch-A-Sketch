c361395b6d31eca5a4b79dbafa533b70d738b2ef
nbguerreiro <nbguerreiro@sapo.pt>
2021-03-10 11:01:29 +0000

    etc

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const container = document.querySelector('.container');
 
-for (var i = 1; i <= 256; i++) {
+let numOfSquares = 100;
+let widthOfSquares = 100 / numOfSquares;
+// console.log(widthOfSquares);
+container.style.gridTemplateColumns = `repeat(${numOfSquares}, ${widthOfSquares}%)`;
+container.style.gridTemplateRows = `repeat(${numOfSquares}, ${widthOfSquares}%)`;
+
+
+for (var i = 1; i <= Math.pow(numOfSquares, 2); i++) {
     let newdiv = document.createElement('div');
     newdiv.classList.add('square');
     container.appendChild(newdiv);
@@ -15,7 +22,7 @@ squares.forEach((s) => {
 })
 
 function restart() {
-    console.log("somthing");
+    console.log("restart");
     squares.forEach((s) => {
         s.style.backgroundColor = null;
     })
